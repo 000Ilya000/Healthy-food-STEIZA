d808de27e1264f6b44f6c50a4f9428106c55c1cc
Илья <92045360+000Ilya000@users.noreply.github.com>
2023-02-06 22:46:43 +0400

    I started making the next block - dishes

--- food-app/src/pages/MainPage.jsx
+++ food-app/src/pages/MainPage.jsx
@@ -16,6 +16,7 @@ import Basicfood2 from '../assets/img/BasicFOOD2.svg';
 // import WorkLine from '../assets/img/WorkLine1.svg';
 import ArrowLeft from '../assets/img/Arrowleft.svg';
 import ArrowRight from '../assets/img/ArrowRight.svg';
+import DISHES from '../assets/img/DISHES.svg';
 
 function Main () {
     return (
@@ -121,6 +122,89 @@ function Main () {
                             </WorkCard>
                         </WorkCards>
                     </HowItWork>
+                    <Dishes>
+                        <DishesTitle>Dish Of The Day</DishesTitle>
+                        <FirstCards>
+                            <DishCard>
+                                <img src="" alt="" />
+                                <DishesCardTitle>
+                                    <MainDishesCardTitle></MainDishesCardTitle>
+                                    <DishesCardTitleDescr></DishesCardTitleDescr>
+                                </DishesCardTitle>
+                                <DishesDescr></DishesDescr>
+                                <DishesCardFoot>
+                                    <DishesReiting></DishesReiting>
+                                    <Button></Button>
+                                </DishesCardFoot>
+                            </DishCard>
+                            
+                            <DishCard>
+                                <img src="" alt="" />
+                                <DishesCardTitle>
+                                    <MainDishesCardTitle></MainDishesCardTitle>
+                                    <DishesCardTitleDescr></DishesCardTitleDescr>
+                                </DishesCardTitle>
+                                <DishesDescr></DishesDescr>
+                                <DishesCardFoot>
+                                    <DishesReiting></DishesReiting>
+                                    <Button></Button>
+                                </DishesCardFoot>
+                            </DishCard>
+                            
+                            <DishCard>
+                                <img src="" alt="" />
+                                <DishesCardTitle>
+                                    <MainDishesCardTitle></MainDishesCardTitle>
+                                    <DishesCardTitleDescr></DishesCardTitleDescr>
+                                </DishesCardTitle>
+                                <DishesDescr></DishesDescr>
+                                <DishesCardFoot>
+                                    <DishesReiting></DishesReiting>
+                                    <Button></Button>
+                                </DishesCardFoot>
+                            </DishCard>
+                        </FirstCards>
+                        <SecondCards>
+                            <DishCard>
+                                <img src="" alt="" />
+                                <DishesCardTitle>
+                                    <MainDishesCardTitle></MainDishesCardTitle>
+                                    <DishesCardTitleDescr></DishesCardTitleDescr>
+                                </DishesCardTitle>
+                                <DishesDescr></DishesDescr>
+                                <DishesCardFoot>
+                                    <DishesReiting></DishesReiting>
+                                    <Button></Button>
+                                </DishesCardFoot>
+                            </DishCard>
+                            
+                            <DishCard>
+                                <img src="" alt="" />
+                                <DishesCardTitle>
+                                    <MainDishesCardTitle></MainDishesCardTitle>
+                                    <DishesCardTitleDescr></DishesCardTitleDescr>
+                                </DishesCardTitle>
+                                <DishesDescr></DishesDescr>
+                                <DishesCardFoot>
+                                    <DishesReiting></DishesReiting>
+                                    <Button></Button>
+                                </DishesCardFoot>
+                            </DishCard>
+                            
+                            <DishCard>
+                                <img src="" alt="" />
+                                <DishesCardTitle>
+                                    <MainDishesCardTitle></MainDishesCardTitle>
+                                    <DishesCardTitleDescr></DishesCardTitleDescr>
+                                </DishesCardTitle>
+                                <DishesDescr></DishesDescr>
+                                <DishesCardFoot>
+                                    <DishesReiting></DishesReiting>
+                                    <Button></Button>
+                                </DishesCardFoot>
+                            </DishCard>
+                        </SecondCards>
+                    </Dishes>
                 </MainM>  
             </MainScreen>
         </>
@@ -505,5 +589,70 @@ const WorkCardDescr = styled.p`
     letter-spacing: -0.1625px;
 
     color: rgba(255, 255, 255, 0.5);
+`;
+
+const Dishes = styled.div`
+
+`;
+
+const DishesTitle = styled.p`
+    min-height:150px;
+    min-width:400px;
+    display: flex;
+    background: url('${DISHES}') no-repeat center center;
+    align-items: center;
+    justify-content: center;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    // line-height: 90px;
+    text-align: center;
+    letter-spacing: 0.2em;
+    text-transform: capitalize;
+    color: #D9D9D9;
+    padding-bottom: 6px;
+    margin-bottom: 0px;
+    flex-direction: column;
+
+    ::after {
+        height: 2px;
+        max-width: 100%;
+        background: #34C759;
+        color: #34C759;
+        content: ' ';
+        min-width:56px;
+        // margin-top:40px;
+    }
+`;
+
+
+
+const FirstCards = styled.div`
+    display: flex;
+`;
+
+const SecondCards = styled.div`
+    display: flex;
+`;
+
+const DishCard = styled.div`
+`;
+
+const DishesCardTitle = styled.div`
+`;
+
+const MainDishesCardTitle = styled.p`
+`;
+
+const DishesCardTitleDescr = styled.p`
+`;
+
+const DishesDescr = styled.p`
+`;
+
+const DishesCardFoot = styled.div`
+`;
+
+const DishesReiting = styled.div`
 `;
 
